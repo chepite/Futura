f1204bc17cdbe71fba96e71e73b9ac804a853761
chepite <julesvictor@telenet.be>
2022-01-26 23:07:59 +0100

    css mobile, tablet desktop basics for #10

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import './style.css';
 import {init} from './js/scroll_test.js';
 import {Navinit} from './js/nav.js';
 import {Armyinit} from './js/army.js';
-import {posterInit} from './js/poster.js';
+//import {posterInit} from './js/poster.js';
 init();
-posterInit();
+//posterInit();
 Navinit();
 Armyinit();
 
--- src/view/text/index.php
+++ src/view/text/index.php
@@ -7,39 +7,56 @@
     <h3 class="header__by">by Slanted</h3>
   </div>
 </section>
-<section class="PaulRenner">
-  <div class="PaulRenner__illuDiv">
+<div class="desktop__paul">
+    <img class="jaar__illu" src="./assets/1927.svg">
+
+  <div class="renner">
+    <section class="PaulRenner">
+    <div class="PaulRenner__illuDiv">
     <img class="PaulRenner__illuDiv--image" src="./assets/Renner-mobile.png">
     <h2 class="PaulRenner__illuDiv--name">Paul Renner</h2>
     <p class="PaulRenner__illuDiv--date">1878 - 1956</p>
-  </div>
-  <div class="PaulRenner__text">
-  <p class="PaulRenner__text--lead"><?php echo ($texts[6]->value)?></p>
-  <p class="PaulRenner__text--body">   <?php echo ($texts[0]->value)?>
-</p>
+    </div>
+    <div class="PaulRenner__text">
+    <p class="PaulRenner__text--lead"><?php echo ($texts[6]->value)?></p>
+    <p class="PaulRenner__text--body">   <?php echo ($texts[0]->value)?>
+    </p>
+    </div>
+    </div>
+  </section>
 </div>
-</section>
+</div>
 <section class="ArmySection">
   <div class="ArmySection__div--anim">
-  <p>Army Army Army Army Army Army</p>
+  <p>Army Army Army Army Army Army Army Army Army Army Army Army Army Army Army Army Army Army Army Army Army Army</p>
   <div>
     <div class="ArmySection__div--anim2">
-  <p>Army Army Army Army Army Army</p>
+  <p>Army Army Army Army Army Army Army Army Army Army Army Army Army Army Army Army Army Army Army Army Army Army</p>
   <div>
   <div class="ArmySection__div--anim">
-  <p>Army Army Army Army Army Army</p>
+  <p>Army Army Army Army Army Army Army Army Army Army Army Army Army Army Army Army Army Army Army Army Army Army</p>
   <div>
   <div class="ArmySection__div--anim2">
-  <p>Army Army Army Army Army Army</p>
+  <p>Army Army Army Army Army Army Army Army Army Army Army Army Army Army Army Army Army Army Army Army Army Army</p>
   <div>
     <div class="ArmySection__div--anim">
-  <p>Army Army Army Army Army Army</p>
+  <p>Army Army Army Army Army Army Army Army Army Army Army Army Army Army Army Army Army Army Army Army Army Army</p>
+  <div>
+     <div class="ArmySection__div--anim2">
+  <p>Army Army Army Army Army Army Army Army Army Army Army Army Army Army Army Army Army Army Army Army Army Army</p>
+  <div>
+    <div class="ArmySection__div--anim">
+  <p>Army Army Army Army Army Army Army Army Army Army Army Army Army Army Army Army Army Army Army Army Army Army</p>
+  <div>
+      <div class="ArmySection__div--anim2">
+  <p>Army Army Army Army Army Army Army Army Army Army Army Army Army Army Army Army Army Army Army Army Army Army</p>
   <div>
 </section>
 <section class="NaziGermany">
 
   <div class="NaziGermany__horizontal">
     <div class="NaziGermany__horizontal--div  NaziGermany__horizontal--1">
+      <div></div>
       <div class="eye">
         <img class="eye__gif" src="./assets/_main.gif">
       </div>
@@ -51,12 +68,14 @@
         <img src="">
         <p>Swipe</p>
       </div>
+      <img class="NaziGermany__horizontal--1--portrait" src="./assets/hitler_portrait.svg">
     </div>
     <div class="NaziGermany__horizontal--div NaziGermany__horizontal--2">
       <div class="NaziGermany__horizontal--2--title">
         <p class="NaziGermany__horizontal--2--titleRenner">Paul Renner's</p>
         <p class="NaziGermany__horizontal--2--titleKultur">Kulturbol </br>
         schewismus?</p>
+        <!-- <p class="NaziGermany__horizontal--2--titleKulturDesktop">Kulturbolschewismus?</p> -->
       </div>
       <div class="NaziGermany__horizontal--2--circle">
         <p>Even before 1932, Renner made his opposition to the Nazis very clear, notably in his book “Kulturbolschewismus?” (Cultural Bolshevism?). He was unable to find a German publisher, so it was published by his Swiss friend Eugen Rentsch.</p>
@@ -74,8 +93,16 @@
   </div>
 </div>
 </section>
-<section class="poster">
-<div class="poster__canvas">
-    <img class="test" src="./assets/Renner-mobile.png">
+<section class="culture">
+<div class="culture__protest">
+  <img class="culture__protest--image" src="./assets/mobile_protest.svg"></img>
+</div>
+<div class="culture__text">
+  <p class="culture__text--text">
+    So the nazis actively used fonts as an expression of culture and representation of their identity. This means that fonts are commonly used as an expression of culture and identity. Which makes Font selection, use of color, and other design choices are directly inspired by the visual language of their cultural surroundings, which reveals typography as cultural expression (Villagomez).</br> </br>
+
+Nikki Villagomez realized intensive visual research across various American signage environments, which proves that typography can be used to reflect unique characteristics of different cultures and that can vary within the same country. Each unique typography landscape is defined by the residents, that see certain type aspects as representational of their day-to-day life. Cultural characteristics, economic status, and geographic location are important factors that influence the use of typography (Villagomez).
+
+  </p>
 </div>
 </section>

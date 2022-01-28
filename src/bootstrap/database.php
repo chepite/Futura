73081cd4e5328c7d377d6e8a57e799b1cfcc0170
chepite <julesvictor@telenet.be>
2022-01-29 00:02:24 +0100

    heroku config vars fix

--- src/bootstrap/database.php
+++ src/bootstrap/database.php
@@ -3,9 +3,9 @@ use Illuminate\Database\Capsule\Manager as Capsule;
 $capsule = new Capsule;
 $capsule->addConnection([
  'driver'    => 'mysql',
- 'host'      => getenv('PHP_DB_HOST') ?: 'ID338673_futura.db.webhosting.be',
- 'database'  => getenv('PHP_DB_DATABASE') ?: 'ID338673_futura',
- 'username'  => getenv('PHP_DB_USERNAME') ?: 'ID338673_futura',
+ 'host'      => getenv('PHP_DB_HOST') ?: 'mysql',
+ 'database'  => getenv('PHP_DB_DATABASE') ?: 'localhost',
+ 'username'  => getenv('PHP_DB_USERNAME') ?: 'root',
  'password'  => getenv('PHP_DB_PASSWORD') ?: 'devine4life',
  'charset'   => 'utf8mb4',
  'collation' => 'utf8mb4_unicode_ci',
